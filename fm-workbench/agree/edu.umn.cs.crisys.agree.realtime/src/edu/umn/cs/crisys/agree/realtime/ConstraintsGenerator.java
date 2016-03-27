9572e8a8004ce16b785d4bf3913b22a1fc7c81f4
john <john@c-76-113-192-202.hsd1.mn.comcast.net>
2016-03-27 11:15:04 -0500

    Update to allow CSL functionality in subsystems. NOTE: all CSL functionality is disabled. Set boolean test to true to enable.

--- fm-workbench/agree/edu.umn.cs.crisys.agree.realtime/src/edu/umn/cs/crisys/agree/realtime/ConstraintsGenerator.java
+++ fm-workbench/agree/edu.umn.cs.crisys.agree.realtime/src/edu/umn/cs/crisys/agree/realtime/ConstraintsGenerator.java
@@ -52,43 +52,45 @@ import com.rockwellcollins.atc.agree.analysis.extentions.AgreeAutomater;
 	
 public class ConstraintsGenerator implements AgreeAutomater{
 
-	int clkCnt = 0;
-	AgreeNode mTopNode;
-	List<AgreeVar> mT_inputs = new ArrayList<>();
-	List<AgreeStatement> mT_assertions = new ArrayList<>();
+	private List<AgreeNode> progAsserts;
 	
 	@Override
 	public AgreeProgram transform(AgreeProgram program) {
-		
-		// Reset any leftovers from previous runs
-		clkCnt = 0;
-		mT_inputs.clear();
-		mT_assertions.clear();
+
+		boolean test = false;
+				
+		if (test)
+		{
+		AgreeNode mTopNode;
 
 		// Make local copies of the final structures that we need to modify
 		mTopNode = program.topNode;
-		mT_inputs.addAll(mTopNode.inputs);
-		mT_assertions.addAll(mTopNode.assertions);
+		
+//		List<AgreeNode> mProgNode = program.agreeNodes;
+		progAsserts = new ArrayList<>();
+		progAsserts.addAll(program.agreeNodes);
 		
 		List<Node> lustreNodes = new ArrayList<>();
 		lustreNodes.addAll(program.globalLustreNodes);
 
-		boolean test = true;
+		// This line needs to go away after making the below if code a function.
+		List<AgreeStatement> mT_assertions = new ArrayList<>();
 		
-		if (test)
-		{
-		addClocks();
-				
+		cslNode nNode = new cslNode(mTopNode,true);
+		nNode.processNodes();
+		mTopNode = nNode.node;
+					
 		lustreNodes = addCSLNodes(lustreNodes);
 		
 		// Only add the calendar nodes if at least one calendar type was created.
-		if (clkCnt > 0) 
+		mT_assertions.addAll(mTopNode.assertions);
+		if (nNode.clkCnt > 0) 
 		{
 			lustreNodes = buildClkNode(lustreNodes);
 			lustreNodes = buildMinNode(lustreNodes);			
 			
 			// assert (next_inc = (0 -> (pre next2)));
-			Expr clkIncrAssertion = new BinaryExpr(new IdExpr("__NEXT_INC"),BinaryOp.EQUAL,new BinaryExpr(new IntExpr(BigInteger.valueOf(0)),BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,new IdExpr("__NEXT".concat(String.valueOf(clkCnt-1))))));
+			Expr clkIncrAssertion = new BinaryExpr(new IdExpr("__NEXT_INC"),BinaryOp.EQUAL,new BinaryExpr(new IntExpr(BigInteger.valueOf(0)),BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,new  IdExpr(String.join("",nNode.lastSubID,"____NEXT",String.valueOf(nNode.lastSubCnt))))));
 			mT_assertions.add(new AgreeStatement("", clkIncrAssertion, null));
 				
 			// check for 'time'
@@ -101,21 +103,23 @@ public class ConstraintsGenerator implements AgreeAutomater{
 				}
 			}				
 		}
-		}
 //		Property commTimingProp = EMFIndexRetrieval.getPropertyDefinitionInWorkspace(
 //				OsateResourceUtil.getResourceSet(), "Timing_Properties::Period");
 		  
 //		long intVal = PropertyUtils.getIntegerValue(program.topNode.compInst, commTimingProp);
 		//PropertyUtils.getEnumLiteral(conn, commTimingProp);
-				
-		mTopNode = new AgreeNode(program.topNode.id, mT_inputs, program.topNode.outputs, program.topNode.locals, 
-				program.topNode.connections, program.topNode.subNodes, mT_assertions, program.topNode.assumptions, 
+		
+		mTopNode = new AgreeNode(program.topNode.id, mTopNode.inputs, mTopNode.outputs, program.topNode.locals, 
+				program.topNode.connections, mTopNode.subNodes, mT_assertions, program.topNode.assumptions, 
 				program.topNode.guarantees, program.topNode.lemmas, program.topNode.clockConstraint, program.topNode.initialConstraint, 
 				program.topNode.clockVar, program.topNode.reference, program.topNode.timing, program.topNode.compInst);
 		
-		AgreeProgram newProgram = new AgreeProgram(program.agreeNodes, lustreNodes, program.globalTypes, mTopNode);
+		AgreeProgram newProgram = new AgreeProgram(progAsserts, lustreNodes, program.globalTypes, mTopNode);
 		
 		return newProgram;
+		}
+		else
+			return program;
 	}
 
 //	Build the calendar nodes
@@ -197,77 +201,12 @@ public class ConstraintsGenerator implements AgreeAutomater{
 		return lustreNodes;
 	}
 
-//assert ((tmr1, fired1, run1, next1) = Time_Funcs__periodic_proc(sys1.Period, sys1.Phase, sys1.Drift, sys1.tte, next_inc, (0 -> (pre tmr1)), (false -> (pre run1)), (-1)));
-
-	private  void addClocks()
-	{
-
-		for(VarDecl lustreVar : mTopNode.outputs)
-		{
-			AgreeVar var = (AgreeVar)lustreVar;
-			if (var.type.toString().endsWith("__Clk_Stat") )
-			{
-
-				// The inputs
-				IdExpr a = new IdExpr(var.id.concat(".Period"));
-				IdExpr b = new IdExpr(var.id.concat(".Phase"));
-				IdExpr c = new IdExpr(var.id.concat(".Drift"));
-				IdExpr d = new IdExpr(var.id.concat(".tte"));
-				IdExpr e = new IdExpr("__NEXT_INC");
-				IdExpr f = new IdExpr("__TMR".concat(String.valueOf(clkCnt)));
-				IdExpr g = new IdExpr(var.id.concat(".run"));
-				
-		
-				// The outputs
-				List<Expr> ids = new ArrayList<>();
-				ids.add(new IdExpr("__TMR".concat(String.valueOf(clkCnt))));
-				ids.add(new IdExpr(var.id.concat(".fired")));
-				ids.add(new IdExpr(var.id.concat(".run")));
-				ids.add(new IdExpr("__NEXT".concat(String.valueOf(clkCnt))));
-		
-				Expr newAssertion;
-				if (clkCnt == 0)
-				{
-					newAssertion = new BinaryExpr(new TupleExpr(ids), BinaryOp.EQUAL,new NodeCallExpr("__CLOCK",a,b,c,d,e,new BinaryExpr(b,BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,f)),new BinaryExpr(new BoolExpr(false),BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,g)),new IntExpr(BigInteger.valueOf(-1))));
-					mT_inputs.add(new AgreeVar("__NEXT_INC", NamedType.INT, null, null));
-				}	
-				else
-				{
-					newAssertion = new BinaryExpr(new TupleExpr(ids), BinaryOp.EQUAL,new NodeCallExpr("__CLOCK",a,b,c,d,e,new BinaryExpr(b,BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,f)),new BinaryExpr(new BoolExpr(false),BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,g)),new IdExpr("__NEXT".concat(String.valueOf(clkCnt - 1)))));
-				}		
-
-				mT_assertions.add(new AgreeStatement("", newAssertion, null));
-
-				mT_inputs.add(new AgreeVar("__TMR".concat(String.valueOf(clkCnt)), NamedType.INT, null, null));
-				mT_inputs.add(new AgreeVar("__RUN".concat(String.valueOf(clkCnt)), NamedType.BOOL, null, null));
-				mT_inputs.add(new AgreeVar("__NEXT".concat(String.valueOf(clkCnt)), NamedType.INT, null, null));
-				
-				clkCnt++;
-			}
-		}
-
-		return;
-	}
 	
 	private List<Node> addCSLNodes(List<Node> lustreNodes)
 	{
 
-		List<AgreeStatement> origAssertions = new ArrayList<>();
-		origAssertions.addAll(mT_assertions);
-
 		List<Node> nodes = new ArrayList<>();
 		nodes.addAll(lustreNodes);
-		
-		for (AgreeStatement nAssertion : origAssertions)
-		{
-			String tst = nAssertion.expr.toString();
-			if (nAssertion.expr.toString().contains(new String("CSL__")))
-			{
-				mT_assertions.add(updateCSLNodeCall(nAssertion));
-				mT_assertions.remove(nAssertion);
-				
-			}
-		}
 
 		for (Node node : nodes)
 		{
@@ -279,47 +218,6 @@ public class ConstraintsGenerator implements AgreeAutomater{
 		}
 		
 		return lustreNodes;
-	}
-	
-	private AgreeStatement updateCSLNodeCall(AgreeStatement assertion)
-	{					
-		List<Expr> mInputs = new ArrayList<>();
-		List<Expr> mOutputs = new ArrayList<>();
-			
-		PrettyPrintVisitor visitor2 = new PrettyPrintVisitor();
-		assertion.expr.accept(visitor2);
-			
-		ExprMapVisitor visitor = new ExprMapVisitor();
-		Expr tmp = assertion.expr.accept(visitor);
-		Expr tmp2 = ((BinaryExpr) tmp).right;
-		Iterator<Expr> iterator = ((NodeCallExpr)tmp2).args.iterator();
-		String nodeName = (((NodeCallExpr)tmp2).node.toString()).substring(3);
-			
-		while (iterator.hasNext()) 
-		{
-			mInputs.add(iterator.next());
-		}
-		mInputs.add(new BinaryExpr(new IntExpr(BigInteger.valueOf(-1)),BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,new IdExpr("__T_REMAIN".concat(String.valueOf(clkCnt))))));			
-		mInputs.add(new IdExpr("__NEXT_INC"));
-		if (clkCnt > 0)
-			mInputs.add(new IdExpr("__NEXT".concat(String.valueOf(clkCnt-1))));	
-		else
-			mInputs.add(new IntExpr(BigInteger.valueOf(-1)));	
-			
-			
-		tmp2 = ((BinaryExpr) tmp).left;
-		mOutputs.addAll(((TupleExpr)tmp2).elements);
-		mOutputs.add(new IdExpr("__T_REMAIN".concat(String.valueOf(clkCnt))));			
-		mOutputs.add(new IdExpr("__NEXT".concat(String.valueOf(clkCnt))));	
-		mT_inputs.add(new AgreeVar("__T_REMAIN".concat(String.valueOf(clkCnt)), NamedType.INT, null, null));
-		mT_inputs.add(new AgreeVar("__NEXT".concat(String.valueOf(clkCnt)), NamedType.INT, null, null));
-		clkCnt++;
-		
-		Expr tmpExpr = new BinaryExpr(new TupleExpr(mOutputs), BinaryOp.EQUAL, new NodeCallExpr(nodeName,mInputs));
-		
-		AgreeStatement newAssertion = new AgreeStatement("",tmpExpr,null);
-
-		return newAssertion;
 	}
 	
 
@@ -361,4 +259,295 @@ public class ConstraintsGenerator implements AgreeAutomater{
 		String nodeName = node.id.toString().substring(3);
 		return new Node(nodeName,mInputs,mOutputs,mLocals,mEquations);
 	}
+	
+	private void reconcileAssertions(AgreeStatement nAssertion, AgreeStatement oAssertion,String nodeID)
+	{
+		// We'll search for the exact version of the old string and the replace it with the updated version
+		String nTmp = oAssertion.expr.accept(new ExprMapVisitor()).toString();
+		
+		for (AgreeNode lNode : progAsserts)
+		{
+			if (lNode.id.equals(nodeID))
+			{
+				List<AgreeStatement> assertions = new ArrayList<>();
+				assertions.addAll(lNode.assertions);
+			
+				for (AgreeStatement lAssertion : lNode.assertions)
+				{
+					String lTmp = lAssertion.expr.accept(new ExprMapVisitor()).toString();
+			
+					if (nTmp.equals(lTmp))
+					{
+						assertions.add(nAssertion);
+						assertions.remove(lAssertion);
+						break;
+					}
+				}
+				AgreeNode newNode = new AgreeNode(lNode.id, lNode.inputs, lNode.outputs, lNode.locals, 
+						lNode.connections, lNode.subNodes, assertions, lNode.assumptions, 
+						lNode.guarantees, lNode.lemmas, lNode.clockConstraint, lNode.initialConstraint, 
+						lNode.clockVar, lNode.reference, lNode.timing, lNode.compInst);
+				progAsserts.remove(lNode);
+				progAsserts.add(newNode);
+				break;
+			}
+		}
+		return;
+	}
+	
+	private void reconcileOutputs(List<AgreeVar> outputs,String nodeID)
+	{
+		for (AgreeNode lNode : progAsserts)
+		{
+			if (lNode.id.equals(nodeID))
+			{
+				List<AgreeVar> newOutputs = new ArrayList<>();
+				
+				for (AgreeVar anOutput : outputs)
+				{
+					newOutputs.add(anOutput);
+				}
+				AgreeNode newNode = new AgreeNode(lNode.id, lNode.inputs, newOutputs, lNode.locals, 
+						lNode.connections, lNode.subNodes, lNode.assertions, lNode.assumptions, 
+						lNode.guarantees, lNode.lemmas, lNode.clockConstraint, lNode.initialConstraint, 
+						lNode.clockVar, lNode.reference, lNode.timing, lNode.compInst);
+				progAsserts.remove(lNode);
+				progAsserts.add(newNode);
+				break;
+			}
+		}
+		return;
+	}
+	
+	private class cslNode {
+		
+		private boolean		topNode;
+		private boolean		updated;
+		private AgreeNode 	node;
+		private int			clkCnt;
+		private String		nodeID;
+		private String		lastSubID;
+		private int	        lastSubCnt;
+		private List<AgreeStatement> assertions = new ArrayList<>();
+		private List<AgreeVar> outputs = new ArrayList<>();
+		private List<AgreeVar> inputs = new ArrayList<>();
+		
+		public cslNode(AgreeNode origNode, boolean top)
+		{
+			node = origNode;
+			nodeID = origNode.id;
+			updated = false;
+			topNode = top;
+			clkCnt = 0;
+			assertions.addAll(origNode.assertions);
+			outputs.addAll(node.outputs);
+			inputs.addAll(node.inputs);
+		}
+		
+		public void processNodes()
+		{
+			List<AgreeNode> subnodes = new ArrayList<>(); //Iterator
+			List<AgreeNode> newnodes = new ArrayList<>();
+			subnodes.addAll(node.subNodes);
+
+			addClocks();
+			fixCSLCalls(clkCnt);
+
+			lastSubCnt = 0;
+			boolean	firstPass = true;;
+			for (AgreeNode node : subnodes)
+			{
+				cslNode nNode = new cslNode(node,false);
+				nNode.processNodes();
+				newnodes.add(nNode.node);
+				if (nNode.updated)
+				{
+					if (firstPass) 
+					{
+						Expr clkIncrAssertion = new BinaryExpr(new IdExpr(nNode.nodeID.concat("____NEXT0")),BinaryOp.EQUAL,new  IdExpr("__NEXT".concat(String.valueOf(clkCnt))));
+						assertions.add(new AgreeStatement("", clkIncrAssertion, null));
+						firstPass = false;
+					}
+					else
+					{
+						Expr clkIncrAssertion = new BinaryExpr(new IdExpr(nNode.nodeID.concat("____NEXT0")),BinaryOp.EQUAL,new  IdExpr(String.join("",lastSubID,"____NEXT",String.valueOf(lastSubCnt))));
+						assertions.add(new AgreeStatement("", clkIncrAssertion, null));						
+					}
+
+					Expr clkIncrAssertion = new BinaryExpr(new IdExpr(nNode.nodeID.concat("____NEXT_INC")),BinaryOp.EQUAL,new  IdExpr("__NEXT_INC"));
+					assertions.add(new AgreeStatement("", clkIncrAssertion, null));						
+
+					// If this is the final subsystem for a system then these two values will be used to create a reference to the minimum next clock 
+					// increment that ripples out of all subsystems of this system.
+					
+					lastSubCnt = nNode.clkCnt;
+					lastSubID  = nNode.nodeID;
+					updated = true;
+				}
+			}
+
+			node = new AgreeNode(node.id, inputs, outputs, node.locals, 
+					node.connections, newnodes, assertions, node.assumptions, 
+					node.guarantees, node.lemmas, node.clockConstraint, node.initialConstraint, 
+					node.clockVar, node.reference, node.timing, node.compInst);
+
+			return;
+		}
+		
+		public void fixCSLCalls(int clkStart)
+		{
+			boolean firstPass = true;
+			
+			List<AgreeStatement> origAssertions = new ArrayList<>();
+			origAssertions.addAll(assertions);
+			
+			for (AgreeStatement nAssertion : origAssertions)
+			{
+				String tst = nAssertion.expr.toString();
+				if (nAssertion.expr.toString().contains(new String("CSL__")))
+				{
+					AgreeStatement assertion = updateCSLNodeCall(nAssertion);
+					assertions.remove(nAssertion);
+					assertions.add(assertion);
+					reconcileAssertions(assertion, nAssertion,node.id);
+					
+					// NEXT_INC is only added once to the outputs. T_REMAINING and NEXT carry the current clkCnt appendage.
+					if (firstPass)
+					{
+						outputs.add(new AgreeVar("__NEXT_INC", NamedType.INT, null, null));
+						String newOutput = "__NEXT".concat(String.valueOf(clkCnt));
+						if (!existingOutput(newOutput))
+						{
+							outputs.add(new AgreeVar(newOutput, NamedType.INT, null, null));
+						}
+						firstPass = false;
+						updated = true;
+					}
+					
+					outputs.add(new AgreeVar("__T_REMAIN".concat(String.valueOf(clkCnt)), NamedType.INT, null, null));
+					outputs.add(new AgreeVar("__NEXT".concat(String.valueOf(clkCnt+1)), NamedType.INT, null, null));
+					clkCnt++;
+				}
+			}
+			
+			reconcileOutputs(outputs,node.id);
+
+			return;
+		}
+		
+		private AgreeStatement updateCSLNodeCall(AgreeStatement assertion)
+		{					
+			List<Expr> mInputs = new ArrayList<>();
+			List<Expr> mOutputs = new ArrayList<>();
+				
+			PrettyPrintVisitor visitor2 = new PrettyPrintVisitor();
+			assertion.expr.accept(visitor2);
+				
+			ExprMapVisitor visitor = new ExprMapVisitor();
+			Expr tmp = assertion.expr.accept(visitor);
+			Expr tmp2 = ((BinaryExpr) tmp).right;
+			Iterator<Expr> iterator = ((NodeCallExpr)tmp2).args.iterator();
+			String nodeName = (((NodeCallExpr)tmp2).node.toString()).substring(3);
+				
+			while (iterator.hasNext()) 
+			{
+				mInputs.add(iterator.next());
+			}
+			mInputs.add(new BinaryExpr(new IntExpr(BigInteger.valueOf(-1)),BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,new IdExpr("__T_REMAIN".concat(String.valueOf(clkCnt))))));			
+			mInputs.add(new IdExpr("__NEXT_INC"));
+			if ((topNode) && (clkCnt == 0))
+				mInputs.add(new IntExpr(BigInteger.valueOf(-1)));	
+			else
+				mInputs.add(new IdExpr("__NEXT".concat(String.valueOf(clkCnt))));	
+				
+				
+			tmp2 = ((BinaryExpr) tmp).left;
+			mOutputs.addAll(((TupleExpr)tmp2).elements);
+			mOutputs.add(new IdExpr("__T_REMAIN".concat(String.valueOf(clkCnt))));			
+			mOutputs.add(new IdExpr("__NEXT".concat(String.valueOf(clkCnt+1))));	
+			
+			Expr tmpExpr = new BinaryExpr(new TupleExpr(mOutputs), BinaryOp.EQUAL, new NodeCallExpr(nodeName,mInputs));
+			
+			AgreeStatement newAssertion = new AgreeStatement("",tmpExpr,assertion.reference);
+
+			return newAssertion;
+		}
+
+		//assert ((tmr1, fired1, run1, next1) = Time_Funcs__periodic_proc(sys1.Period, sys1.Phase, sys1.Drift, sys1.tte, next_inc, (0 -> (pre tmr1)), (false -> (pre run1)), (-1)));
+
+		private  void addClocks()
+		{
+			for(VarDecl lustreVar : node.outputs)
+			{
+				AgreeVar var = (AgreeVar)lustreVar;
+				if (var.type.toString().endsWith("__Clk_Stat") )
+				{
+					// The inputs
+					IdExpr a = new IdExpr(var.id.concat(".Period"));
+					IdExpr b = new IdExpr(var.id.concat(".Phase"));
+					IdExpr c = new IdExpr(var.id.concat(".Drift"));
+					IdExpr d = new IdExpr(var.id.concat(".tte"));
+					IdExpr e = new IdExpr("__NEXT_INC");
+					IdExpr f = new IdExpr("__TMR".concat(String.valueOf(clkCnt)));
+					IdExpr g = new IdExpr(var.id.concat(".run"));
+					
+					// The outputs
+					List<Expr> ids = new ArrayList<>();
+					ids.add(new IdExpr("__TMR".concat(String.valueOf(clkCnt))));
+					ids.add(new IdExpr(var.id.concat(".fired")));
+					ids.add(new IdExpr(var.id.concat(".run")));
+					ids.add(new IdExpr("__NEXT".concat(String.valueOf(clkCnt+1))));
+			
+					Expr newAssertion;
+					if ((clkCnt == 0) && topNode)
+					{
+						newAssertion = new BinaryExpr(new TupleExpr(ids), BinaryOp.EQUAL,new NodeCallExpr("__CLOCK",a,b,c,d,e,new BinaryExpr(b,BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,f)),new BinaryExpr(new BoolExpr(false),BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,g)),new IntExpr(BigInteger.valueOf(-1))));
+					}	
+					else
+					{
+						newAssertion = new BinaryExpr(new TupleExpr(ids), BinaryOp.EQUAL,new NodeCallExpr("__CLOCK",a,b,c,d,e,new BinaryExpr(b,BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,f)),new BinaryExpr(new BoolExpr(false),BinaryOp.ARROW,new UnaryExpr(UnaryOp.PRE,g)),new IdExpr("__NEXT".concat(String.valueOf(clkCnt)))));
+					}		
+
+					assertions.add(new AgreeStatement("", newAssertion, null));
+
+					inputs.add(new AgreeVar("__TMR".concat(String.valueOf(clkCnt)), NamedType.INT, null, null));
+					inputs.add(new AgreeVar("__RUN".concat(String.valueOf(clkCnt)), NamedType.BOOL, null, null));
+					
+					clkCnt++;
+					inputs.add(new AgreeVar("__NEXT".concat(String.valueOf(clkCnt)), NamedType.INT, null, null));
+				}
+			}
+
+			return;
+		}
+
+		
+		private boolean existingOutput(String name)
+		{
+			boolean found = false;
+			
+			for (AgreeVar anOutput : outputs)
+			{
+				if (anOutput.id.equals(name))
+				{
+					found = true;
+					break;
+				}
+			}
+			
+			if (!found)
+			{
+				for (AgreeVar anInput : inputs)
+				{
+					if (anInput.id.equals(name))
+					{
+						found = true;
+						break;
+					}
+				}
+			}
+			return found;
+		}
+	}
+
 }
